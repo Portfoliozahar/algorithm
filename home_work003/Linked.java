44476848da1e97af1cb0bf12b589697ac4acac9c
Borodinets <assassqwas@gmail.com>
2023-04-21 10:32:08 +0500

    5

---
+++ home_work003/Linked.java
@@ -0,0 +1,43 @@
+package home_work003;
+
+public class Linked {
+    Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+    }
+
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public void reverse() {
+        Node prevNode = null;
+        Node currentNode = head;
+        Node nextNode = null;
+
+        while (currentNode != null) {
+            nextNode = currentNode.next;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        head = prevNode;
+    }
+}
---
+++ home_work003/Program003.java
@@ -0,0 +1,21 @@
+
+package home_work003;
+
+public class Program003 {
+    public static void main(String[] args) {
+        Linked list = new Linked();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.print("Оригинальный список: ");
+        list.printList();
+
+        list.reverse();
+
+        System.out.print("Развёрнутый список: ");
+        list.printList();
+    }
+}
