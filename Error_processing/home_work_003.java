c7c0fdb322fba8658e3400754b4073a5e124b07f
Borodinets <assassqwas@gmail.com>
2023-05-02 18:26:47 +0500

    03

---
+++ Error_processing/home_work_003.java
@@ -0,0 +1,12 @@
+package Error_processing;
+
+public class home_work_003 {
+
+    public static void main(String[] args) {
+        // task1(); //
+        // task2(); //
+        // task3(); //
+        // task4(); //
+    }
+
+}
